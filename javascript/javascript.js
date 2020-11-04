b995cfa7816346250eab7371d05a94930862c344
FrancescoGalanti <galanti_francesco>
2020-11-04 09:37:23 +0100

    fixing

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -32,62 +32,58 @@
   //         `
   //     );
 
-  const classe = [   "francesco",   "chiara",   "marco", "davide",   "Federico",  ];
-
-  const totaleNomi = classe.length;
-
-  // ask user the range
-
-  const min = parseInt(prompt("Scegli un numero dell indice da 0 a " + totaleNomi) );
-  const max = parseInt(prompt("Scegli un altro numero dell indice maggiore od uguale al precedente compreso fra 0 e " + totaleNomi) );
-
-  // generate a new array
-
-  const list = classe.filter((element, index) => {
-
-      if (index >= min && index <= max){
-          return element
-      }
-  });
-
-  console.log(list);
+  // const classe = [   "francesco",   "chiara",   "marco", "davide",   "Federico",  ];
+  //
+  // const totaleNomi = classe.length;
+  //
+  // // ask user the range
+  //
+  // const min = parseInt(prompt("Scegli un numero dell indice da 0 a " + totaleNomi) );
+  // const max = parseInt(prompt("Scegli un altro numero dell indice maggiore od uguale al precedente compreso fra 0 e " + totaleNomi) );
+  //
+  // // generate a new array
+  //
+  // const list = classe.filter((element, index) => {
+  //
+  //     if (index >= min && index <= max){
+  //         return element
+  //     }
+  // });
+  //
+  // console.log(list);
 
 
     // terzo esercizio //
 
-//     const condominio = [
-//     {famiglia: 'Rossi', componenti: 4},
-//     {famiglia: 'Verdi', componenti: 2},
-//     {famiglia: 'Bianchi', componenti: 4},
-//     {famiglia: 'Gialli', componenti: 8}
-//   ]
-//   let condscala = [];
-//
-//   condscala = condominio.map( (element) => {
-//     return {
-//         ...element, // QUI USO LO SPREAD
-//         scala: generateScala()
-//         }
-// });
-//
-//
-//
-//
-//   console.table(condominio);
-//   console.table(condscala);
-//
-//   //Function Scala
-//
-//   function generateScala() {
-//     let scale = 'abc';
-//     let scala = scale[randScala(0, scale.length - 1)];
-//
-//     return scala;
-//   }
-//
-//   function randScala(min,max) {
-//     return Math.floor (Math.random() * max - min + 1 ) + min;
-//   }
+    var myarray = [
+    {name: "Francesco", type:"tshirt", color:"red"},
+    {name: "Dario", type: "scooter", color: "blue"},
+    {name: "CrissCross", type: "scarpe", color: "yellow"},
+    {name: "Jenny", type: "borsa", color:"white"},
+]
+
+const newarray = myarray.map(element => {
+    return {
+        ...element,
+        position: generatePosition()
+    }
+});
+
+console.table(newarray);
+
+
+
+
+
+
+function generatePosition(){
+  var letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+  var letter = letters[Math.floor(Math.random() * (letters.length - 1) + 1)];
+
+  return letter;
+}
+
 
 
 
